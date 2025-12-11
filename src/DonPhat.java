c0a0253a160768bd1931e02c73adf7a36d5efd6c
ThietKeHDT <lietthuan54@gmail.com>
2025-12-11 10:02:28 +0700

    thuanvuu

---
+++ src/DonPhat.java
@@ -0,0 +1,18 @@
+public class DonPhat {
+    private SinhVien SVNT;
+    private int soNgay;
+    private int soTien;
+    private boolean trangThai;
+
+    public DonPhat() {
+    }
+
+    public DonPhat(SinhVien SVNT, int soNgay, int soTien, boolean trangThai) {
+        this.SVNT = SVNT;
+        this.soNgay = soNgay;
+        this.soTien = soTien;
+        this.trangThai = trangThai;
+    }
+
+
+}
